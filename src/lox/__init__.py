81036ccf5592e4fcb282f5e02a1afdbf74fbc88a
Michihiro Nakamura <michihiro.nakamura@uzabase.com>
2023-11-20 22:54:36 +0900

    initial commit

---
+++ src/lox/__init__.py
@@ -0,0 +1,40 @@
+import sys
+
+from lox.scanner import Scanner
+import lox.error as error
+
+
+def main():
+    if len(sys.argv) > 2:
+        print("Usage: lox [script]")
+        sys.exit(64)
+    elif len(sys.argv) == 2:
+        run_file(sys.argv[0])
+    else:
+        run_prompt()
+
+
+def run_file(path: str):
+    with open(path) as f:
+        s = f.read()
+        run(s)
+    if error.had_error:
+        sys.exit(65)
+
+
+def run_prompt():
+    global had_error
+    while True:
+        s = input("> ")
+        if s == "":
+            break
+        run(s)
+        error.had_error
+
+
+def run(source: str):
+    scanner = Scanner(source)
+    tokens = scanner.scan_tokens()
+
+    for token in tokens:
+        print(token)
---
+++ src/lox/token_type.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Any
+
+
+class TokenType(Enum):
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
+
+    BANG = auto()
+    BANG_EQUAL = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
+
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
+
+    AND = auto()
+    CLASS = auto()
+    ELSE = auto()
+    FALSE = auto()
+    FUN = auto()
+    FOR = auto()
+    IF = auto()
+    NIL = auto()
+    OR = auto()
+    PRINT = auto()
+    RETURN = auto()
+    SUPER = auto()
+    THIS = auto()
+    TRUE = auto()
+    VAR = auto()
+    WHILE = auto()
+
+    EOF = auto()
+
+
+@dataclass(frozen=True)
+class Token:
+    type: TokenType
+    lexeme: str
+    literal: Any | None
+    line: int
+
+    def __str__(self):
+        return f"{self.type} {self.lexeme} {self.literal}"
