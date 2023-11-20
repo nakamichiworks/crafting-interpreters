276d7a89507e05a9a2cf04ba157bf5dbfe49d762
Michihiro Nakamura <michihiro.nakamura@uzabase.com>
2023-11-21 01:05:53 +0900

    debug

--- src/lox/__init__.py
+++ src/lox/__init__.py
@@ -9,7 +9,7 @@ def main():
         print("Usage: lox [script]")
         sys.exit(64)
     elif len(sys.argv) == 2:
-        run_file(sys.argv[0])
+        run_file(sys.argv[1])
     else:
         run_prompt()
 
